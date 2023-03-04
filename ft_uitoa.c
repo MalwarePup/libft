ee43032156d97245371c2c7f2ca3eb3d0113267e
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2023-03-05 00:34:53 +0100

    feat(ft_uitoa.c): Add ft_uitoa to the project

---
+++ ft_uitoa.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_uitoa.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/02 20:23:49 by ladloff           #+#    #+#             */
+/*   Updated: 2023/02/28 23:31:44 by ladloff          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_uitoa(unsigned int n)
+{
+	unsigned int	value;
+	char			*buffer;
+	int				buffer_size;
+
+	if (n == 0)
+	{
+		buffer = malloc(2 * sizeof(char));
+		if (!buffer)
+			return (NULL);
+		return ((buffer[0] = '0'), (buffer[1] = '\0'), buffer);
+	}
+	value = n;
+	buffer_size = 0;
+	while (value > 0 && ++buffer_size)
+		value /= 10;
+	buffer = malloc((buffer_size + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	buffer[buffer_size] = '\0';
+	while (--buffer_size >= 0)
+	{
+		buffer[buffer_size] = '0' + (n % 10);
+		n /= 10;
+	}
+	return (buffer);
+}
