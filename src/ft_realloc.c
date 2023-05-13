31f246a6013a61483b8b78befb75ba7f4ec15be6
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2023-05-13 19:10:42 +0200

    feat(realloc): improve realloc function

--- src/ft_realloc.c
+++ src/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 22:25:01 by ladloff           #+#    #+#             */
-/*   Updated: 2023/05/07 11:55:49 by ladloff          ###   ########.fr       */
+/*   Updated: 2023/05/13 19:07:44 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 void	*ft_realloc(void *ptr, size_t current_size, size_t new_size)
 {
 	void	*new_ptr;
+	size_t	copy_size;
 
 	if (ptr == NULL)
 		return (malloc(new_size));
@@ -24,16 +25,16 @@ void	*ft_realloc(void *ptr, size_t current_size, size_t new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size <= current_size)
+	if (new_size == current_size)
 		return (ptr);
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (NULL);
-	if (ft_memcpy(new_ptr, ptr, current_size) == NULL)
-	{
-		free(new_ptr);
-		return (NULL);
-	}
+	if (new_size < current_size)
+		copy_size = new_size;
+	else
+		copy_size = current_size;
+	ft_memcpy(new_ptr, ptr, copy_size);
 	free(ptr);
 	return (new_ptr);
 }
