4b0ff2fd495cd509e5ed68c19e303a9f6d7b9a70
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-02-15 17:38:01 +0100

    refactor(ft_strjoin.c): optimize memory allocation and free operations in ft_strjoin2 function fix(ft_strjoin.c): handle null s1 string in ft_strjoin2 to prevent segmentation fault

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 13:03:22 by ladloff           #+#    #+#             */
-/*   Updated: 2024/02/15 10:39:08 by ladloff          ###   ########.fr       */
+/*   Updated: 2024/02/15 17:37:21 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,26 +67,26 @@ char	*ft_strjoin2(char *s1, char *s2)
 	size_t	len_total;
 	char	*strjoin;
 
-	if (!s1 || !s2)
+	if (!s2)
 		return (NULL);
-	len_total = ft_strlen(s1) + ft_strlen(s2);
+	if (!s1)
+		len_total = ft_strlen(s2);
+	else
+		len_total = ft_strlen(s1) + ft_strlen(s2);
 	strjoin = malloc((len_total + 1) * sizeof(char));
 	if (!strjoin)
-	{
-		free(s1);
-		free(s2);
-		return (NULL);
-	}
+		return (free(s1), free(s2), NULL);
 	i = -1;
-	while (s1[++i])
-		strjoin[i] = s1[i];
+	if (s1)
+		while (s1[++i])
+			strjoin[i] = s1[i];
+	else
+		i = 0;
 	j = -1;
 	while (i + ++j < len_total)
 		strjoin[i + j] = s2[j];
 	strjoin[i + j] = '\0';
-	free(s1);
-	free(s2);
-	return (strjoin);
+	return (free(s1), free(s2), strjoin);
 }
 
 char	*ft_strjoin3(char *s1, char *s2)
