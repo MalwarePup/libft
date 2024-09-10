f8182a14d6d5ac855bb7ed87174a7356dbc41f55
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-09-10 11:59:08 +0200

    feat(ft_atof.c): add support for scientific notation in ft_atof function fix(ft_atof.c): handle special cases for "inf", "infinity", and "nan" strings refactor(ft_atof.c): split fraction parsing into a separate function for clarity

--- src/ft_atof.c
+++ src/ft_atof.c
@@ -6,37 +6,76 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 00:06:34 by ladloff           #+#    #+#             */
-/*   Updated: 2023/05/27 00:43:06 by ladloff          ###   ########.fr       */
+/*   Updated: 2024/09/10 11:57:26 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 #include "libft.h"
 
-double	ft_atof(const char *str)
+static double	handle_exponent(const char *str, double result)
 {
-	int8_t	sign;
+	int		exponent;
+	int8_t	exp_sign;
+
+	exponent = 0;
+	exp_sign = 1;
+	if (*str == 'e' || *str == 'E')
+	{
+		str++;
+		if (*str == '-' || *str == '+')
+		{
+			if (*str++ == '-')
+				exp_sign = -1;
+		}
+		while (*str && *str >= '0' && *str <= '9')
+			exponent = exponent * 10 + (*str++ - '0');
+		result *= pow(10.0, exp_sign * exponent);
+	}
+	return (result);
+}
+
+static double	parse_fraction(const char **str)
+{
 	double	factor;
 	double	result;
 
 	result = 0.0;
-	sign = 1;
 	factor = 1.0;
-	while (ft_isspace(*str))
+	if (**str == '.')
+	{
+		(*str)++;
+		while (**str && **str >= '0' && **str <= '9')
+		{
+			result = result * 10.0 + (**str - '0');
+			factor *= 10.0;
+			(*str)++;
+		}
+	}
+	return (result / factor);
+}
+
+double	ft_atof(const char *str)
+{
+	double	result;
+	int8_t	sign;
+
+	if (!str)
+		return (0.0);
+	result = 0.0;
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
 		str++;
 	if (*str == '-' || *str == '+')
 		if (*str++ == '-')
-			sign = -sign;
-	while (ft_isdigit(*str))
+			sign = -1;
+	if (ft_strcmp(str, "inf") == 0 || ft_strcmp(str, "infinity") == 0)
+		return (sign * INFINITY);
+	if (ft_strcmp(str, "nan") == 0)
+		return (NAN);
+	while (*str >= '0' && *str <= '9')
 		result = result * 10.0 + (*str++ - '0');
-	if (*str == '.')
-	{
-		str++;
-		while (ft_isdigit(*str))
-		{
-			result = result * 10.0 + (*str++ - '0');
-			factor *= 10.0;
-		}
-	}
-	return (sign * result / factor);
+	result += parse_fraction(&str);
+	return (handle_exponent(str, result * sign));
 }
